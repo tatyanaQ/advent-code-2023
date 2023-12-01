a8309a2663ee22275b0d60e6e485dba52ba2ca1a
tslabodchykava <tatyana.slabodchykava@gmail.com>
2023-12-01 20:14:50 +0200

    day 1, part 2 with regexp

--- day1/part2.js
+++ day1/part2.js
@@ -1,88 +1,44 @@
 const { readFile, arraySum } = require("../utils");
 
-const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const digitNamesMap = {
-  one: "1",
-  two: "2",
-  three: "3",
-  four: "4",
-  five: "5",
-  six: "6",
-  seven: "7",
-  eight: "8",
-  nine: "9",
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
 };
-const digitNames = Object.keys(digitNamesMap);
 
-const getIsDigit = (char) => digits.includes(char);
-
-function getFirstDigit(line) {
-  const chars = line.split("");
-  let result = "";
-
-  chars.find((char, index) => {
-    const isDigit = getIsDigit(char);
-    if (isDigit) {
-      result = char;
-      return true;
-    }
-
-    const isDigitName = digitNames.some((digitName) => {
-      const isStartOfDigitName =
-        line.slice(index, index + digitName.length) === digitName;
-      if (isStartOfDigitName) {
-        result = digitNamesMap[digitName];
-        return true;
-      }
-
-      return false;
-    });
-    if (isDigitName) {
-      return true;
-    }
-
-    return false;
-  });
-
-  return result;
-}
-
-function getLastDigit(line) {
-  const chars = line.split("");
-  let result = "";
-
-  chars.findLast((char, index) => {
-    const isDigit = getIsDigit(char);
-    if (isDigit) {
-      result = char;
-      return true;
-    }
-
-    const isDigitName = digitNames.some((digitName) => {
-      const isEndOfDigitName =
-        line.slice(index - digitName.length + 1, index + 1) === digitName;
-      if (isEndOfDigitName) {
-        result = digitNamesMap[digitName];
-        return true;
-      }
-
-      return false;
-    });
-    if (isDigitName) {
-      return true;
-    }
-
-    return false;
-  });
-
-  return result;
-}
+const digitsRegExps = Object.keys(digitNamesMap).map(
+  (digit) => new RegExp(digit, "g")
+);
 
 function getLineCalibrationValue(line) {
-  const firstDigit = getFirstDigit(line);
-  const lastDigit = getLastDigit(line);
+  const matches = digitsRegExps
+    // [[digit, index]], e.g. [['two', 0]]
+    .flatMap((pattern) => Array.from(line.matchAll(pattern)))
+    .filter((match) => !!match?.[0])
+    .sort((match1, match2) => match1.index - match2.index);
 
-  return Number(`${firstDigit}${lastDigit}`);
+  const firstDigit = matches[0][0];
+  const lastDigit = matches[matches.length - 1][0];
+
+  const value = digitNamesMap[firstDigit] * 10 + digitNamesMap[lastDigit];
+
+  return value;
 }
 
 function solvePuzzle(inputFileName) {
