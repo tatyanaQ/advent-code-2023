5f5d5d99f755dcc2bb12ee36cab380ca3cbb5ce5
tslabodchykava <tatyana.slabodchykava@gmail.com>
2023-12-02 12:50:49 +0200

    array mutiplication

--- utils.js
+++ utils.js
@@ -13,6 +13,10 @@ function arraySum(array) {
   return array.reduce((sum, item) => item + sum, 0);
 }
 
+function arrayMultiplication(array) {
+  return array.reduce((multiplication, item) => item * multiplication, 1);
+}
+
 function trimValues(array) {
   return array.map((value) => value.trim());
 }
@@ -20,4 +24,5 @@ function trimValues(array) {
 module.exports = {
   readFile,
   arraySum,
+  arrayMultiplication,
 };
