f36873b10e042e84fa29c11b5e1dac87261b7abe
tslabodchykava <tatyana.slabodchykava@gmail.com>
2023-12-02 12:36:58 +0200

    updating barebone

--- barebone.js
+++ barebone.js
@@ -1,7 +1,7 @@
 const { readFile } = require("./utils");
 
 function solvePuzzle(inputFileName) {
-  const lines = readFile({ fileName: inputFileName });
+  const lines = readFile({ fileName: inputFileName, trimmed: true });
   console.log(lines);
 }
 
